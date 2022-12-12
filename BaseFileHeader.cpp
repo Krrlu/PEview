37e981ef31964cb288d455d20520abbf50fb12b2
Krrlu <kla251@sfu.ca>
2022-12-12 13:02:57 -0800

    Changed variable and function names

--- BaseFileHeader.cpp
+++ BaseFileHeader.cpp
@@ -2,7 +2,7 @@
 #include "BaseFileHeader.h"
 
 
-void errorleave(int i) {
+void errorreturn(int i) {
 	switch (i) {
 	case -1:
 		printf("Error, cannot open the file");
@@ -67,31 +67,31 @@ void printtime(time_t stamp) {
 void fileHeader(LPCSTR fileLocaion) {
 	HANDLE fileHandle, mappingHandle;
 	LPVOID mapPointer;
-	IMAGE_FILE_HEADER Imagefh;
+	IMAGE_FILE_HEADER coffheader;
 
 	fileHandle = CreateFileA(fileLocaion, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (fileHandle == INVALID_HANDLE_VALUE) {
-		errorleave(-1);
+		errorreturn(-1);
 	}
 
 	mappingHandle = CreateFileMappingA(fileHandle,NULL, PAGE_READONLY,0,0,NULL);
 	if (mappingHandle == NULL) {
-		errorleave(-1);
+		errorreturn(-1);
 	}
 
 	mapPointer = MapViewOfFile(mappingHandle, FILE_MAP_READ, 0, 0, 0);
 	if (mapPointer == NULL) {
-		errorleave(-1);
+		errorreturn(-1);
 	}
 	if (*((WORD*)mapPointer) != IMAGE_DOS_SIGNATURE){
-		errorleave(-2);
+		errorreturn(-2);
 	}
 	int offsetPE = *(DWORD*)((BYTE*)mapPointer + 0x3c);
 	if (*(DWORD*)((BYTE*)mapPointer + offsetPE) != IMAGE_NT_SIGNATURE) {
-		errorleave(-2);
+		errorreturn(-2);
 	}
-	Imagefh = *(IMAGE_FILE_HEADER*)((BYTE*)mapPointer + offsetPE + 4);
-	pmachinetype(Imagefh.Machine);
-	printtime(Imagefh.TimeDateStamp);
+	coffheader = *(IMAGE_FILE_HEADER*)((BYTE*)mapPointer + offsetPE + 4);
+	pmachinetype(coffheader.Machine);
+	printtime(coffheader.TimeDateStamp);
 }
 
